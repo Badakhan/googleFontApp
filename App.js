4d51fbb020bbbc07c318a1b30ee85e9316ce7e22
Mohammed Nasser <mohammednasser@MacBook-Air.local>
2020-08-17 06:23:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState  } from "react";
+import 'react-native-gesture-handler';
+import {View , Text , StyleSheet } from 'react-native'; 
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+async function loadResourcesAsync() {
+  await Promise.all([
+   
+    Font.loadAsync({
+      Bold: require('./assets/fonts/LibreBaskerville-Bold.ttf'),
+      Italic: require('./assets/fonts/LibreBaskerville-Italic.ttf'),
+      Regular: require('./assets/fonts/LibreBaskerville-Regular.ttf'),
+      
+    }),
+  ]);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    
+
+
+function handleLoadingError(error) {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+  
+
+
+export default (props) => {
+
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } 
+
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontFamily: 'Regular' , fontSize: 18  }}>Home Screen</Text>
+        </View>
+      );
+    }
+    
+   
+    
